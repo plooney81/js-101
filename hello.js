9b46e763471992b1f50775e5ccc3079d1cbf3833
Pete Looney <petelooney81@gmail.com>
2020-09-08 11:36:23 -0500

    tipCalc functions added

--- hello.js
+++ hello.js
@@ -9,3 +9,25 @@ function madLib(name = 'Pete', subject = 'Stem') {
     return newString;
 }
 console.log(madLib());
+
+function tipCalcOne(bill, service) {
+    if (service === 'good'){
+        return Math.round(bill * .2);
+    } else if (service == 'fair'){
+        return Math.round(bill * .15);
+    } else if (service == 'bad') {
+        return Math.round(bill * .1);
+    } else {
+        console.log('Something else happened')
+    }
+}
+
+function tipCalcTwo(bill, service) {
+    return tipCalcOne(bill, service) + bill
+}
+
+function tipCalcThree(bill, service='good', participants){
+    return Math.ceil(tipCalcTwo(bill, service) / participants)
+}
+const example = tipCalcThree(40, 'fair', 2)
+console.log(example)
