5769e4a25e9e5184e7b65d1fc58cade375b31295
Pete Looney <petelooney81@gmail.com>
2020-09-08 13:26:04 -0500

    added printNumbers, printSquare, and printBox functions

--- hello.js
+++ hello.js
@@ -2,13 +2,13 @@ function hello(name = 'world') {
     console.log('Hello, ' + name + '!');
 }
 // prompt('Whats your name?') cant use this because it doesn't exist for node
-hello();
+// hello();
 
 function madLib(name = 'Pete', subject = 'Stem') {
     const newString = name + '\'s favorite subject in school is ' + subject + '.';
     return newString;
 }
-console.log(madLib());
+// console.log(madLib());
 
 function tipCalcOne(bill, service) {
     if (service === 'good'){
@@ -30,4 +30,31 @@ function tipCalcThree(bill, service='good', participants){
     return Math.ceil(tipCalcTwo(bill, service) / participants)
 }
 const example = tipCalcThree(40, 'fair', 2)
-console.log(example)
+// console.log(example)
+
+function printNumbers(start, end){
+    for (let i = start; i <= end; i++) {
+        console.log(i);
+    }
+}
+// printNumbers(1,10);
+
+function printSquare(number){
+    for (let i = 0; i < number; i++){
+        // repeat will repeat the specified string the number of times you tell it to
+        console.log('-'.repeat(number))
+    }
+}
+// printSquare(5);
+
+function printBox(width, height){
+    // Top of box
+    for(let i = 1; i <= height; i++){
+        if(i === 1 || i === height){
+            console.log(' ' + '-'.repeat(width - 2) + ' ')
+        }else{
+            console.log('|' + ' '.repeat(width-2) + '|')
+        }
+    }
+}
+// printBox(6, 4)
