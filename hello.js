bc7126881d0900d523446dc2b6167eafeba5583e
Pete Looney <petelooney81@gmail.com>
2020-09-08 13:38:05 -0500

    added printBanner function

--- hello.js
+++ hello.js
@@ -58,3 +58,14 @@ function printBox(width, height){
     }
 }
 // printBox(6, 4)
+
+function printBanner(text){
+    for (let i = 0; i < 3; i++){
+        if (i === 0 || i === 2){
+            console.log('-'.repeat(text.length + 4));
+        } else {
+            console.log('- ' + text + ' -');
+        }
+    }
+}
+printBanner('Welcome to DigitalCrafts')
