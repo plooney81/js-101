66392330b8bd46ab291d531bd3a5f49aeeb26821
Pete Looney <petelooney81@gmail.com>
2020-09-08 14:03:17 -0500

    added leetSpeak function

--- hello.js
+++ hello.js
@@ -68,4 +68,40 @@ function printBanner(text){
         }
     }
 }
-printBanner('Welcome to DigitalCrafts')
+// printBanner('Welcome to DigitalCrafts')
+// 
+function leetSpeak(string){
+    string = string.toLowerCase();
+    let newString = '';
+    for (let i = 0; i < string.length; i++){
+        switch (string[i]){
+            case 'a':
+                newString += '4';
+                break;
+            case 'e':
+                newString += '3';
+                break;
+            case 'g':
+                newString += '6';
+                break;
+            case 'i':
+                newString += '1';
+                break;
+            case 'o':
+                newString += '0';
+                break;
+            case 's':
+                newString += '5';
+                break;
+            case 't':
+                newString += '7';
+                break;
+            default:
+                newString += string[i];
+        }
+        
+    }
+    console.log(newString);
+}
+
+leetSpeak('Leet');
