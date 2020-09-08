db4f8084ea93c0e571a92caa3727fe9db784bd39
Pete Looney <petelooney81@gmail.com>
2020-09-08 11:20:35 -0500

    added madlib function

--- hello.js
+++ hello.js
@@ -3,3 +3,9 @@ function hello(name = 'world') {
 }
 // prompt('Whats your name?') cant use this because it doesn't exist for node
 hello();
+
+function madLib(name = 'Pete', subject = 'Stem') {
+    const newString = name + '\'s favorite subject in school is ' + subject + '.';
+    return newString;
+}
+console.log(madLib());
